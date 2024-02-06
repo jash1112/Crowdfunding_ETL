611f124b94ebdf25e4714453add6978e734c51e9
Jash Bikash <68443738+jash1112@users.noreply.github.com>
2024-02-05 23:58:27 -0500

    Update crowdfunding_db_schema.sql

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,36 +1,45 @@
 -- crowdfunding_db_schema.sql
 
 -- Create categories table
+DROP crowdfunding_db_schema.category;
 CREATE TABLE crowdfunding_db_schema.category (
-    category_id SERIAL PRIMARY KEY,
+    category_id VARCHAR(255) PRIMARY KEY,
     category VARCHAR(255) NOT NULL
 );
 
 -- Create subcategories table
 drop table crowdfunding_db_schema.subcategory;
 CREATE TABLE crowdfunding_db_schema.subcategory (
-    subcategory_id SERIAL PRIMARY KEY,
+    subcategory_id VARCHAR(255) PRIMARY KEY,
     subcategory VARCHAR(255) NOT NULL
 );
 
 -- Create contacts table
+DROP TABLE crowdfunding_db_schema.contacts;
 CREATE TABLE crowdfunding_db_schema.contacts (
-    contact_id SERIAL PRIMARY KEY,
+    contact_id INT PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
 	last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL
 );
-FOREIGN KEY (category_id) REFERENCES crowdfunding_db_schema.category(category_id)
 
--- Create Projects table
-CREATE TABLE Projects (
-    project_id SERIAL PRIMARY KEY,
-    project_name VARCHAR(255) NOT NULL,
-    description TEXT,
-    user_id INT,
-    category_id INT,
-    subcategory_id INT,
-    FOREIGN KEY (user_id) REFERENCES Users(user_id),
-    FOREIGN KEY (category_id) REFERENCES Categories(category_id),
-    FOREIGN KEY (subcategory_id) REFERENCES Subcategories(subcategory_id)
+-- Create campaign table
+CREATE TABLE crowdfunding_db_schema.campaign (
+    cf_id INT PRIMARY KEY,
+    contact_id INT,
+    company_name VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    goal INT,
+    pledged INT,
+	outcome VARCHAR(255) NOT NULL,
+	backers_count VARCHAR(255) NOT NULL,
+	country VARCHAR(255) NOT NULL,
+	currency VARCHAR(255) NOT NULL,
+	launched_date INT,
+	end_date INT,
+	category_id VARCHAR (255) NOT NULL,
+	subcategory_id VARCHAR(255) NOT NULL,
+    FOREIGN KEY (contact_id) REFERENCES crowdfunding_db_schema.contacts(contact_id),
+    FOREIGN KEY (category_id) REFERENCES crowdfunding_db_schema.category(category_id),
+    FOREIGN KEY (subcategory_id) REFERENCES crowdfunding_db_schema.subcategory(subcategory_id)
 );
